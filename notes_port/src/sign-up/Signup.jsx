5b96e091b53a9da9ff36e72fbe93ad3a3520b255
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-06-20 13:03:09 +0300

    progress

---
+++ notes_port/src/sign-up/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Signup.css"
+const Signup = () => {
+    return (
+        <div>SomaSoma</div>
+    )
+}
+
+export default Signup
---
+++ pdfViewer/js/main.js
@@ -0,0 +1,20 @@
+const url = '../docs/SOMASOMA.pdf';
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.5,
+    canvas = document.querySelector('#pdf-render'),
+    ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+};
+
+//Get the document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+});
